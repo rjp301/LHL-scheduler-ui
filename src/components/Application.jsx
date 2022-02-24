afe50c9cbc44323dd95f0ff9c8dc0f871e0fb468
rjp301 <rileypaul96@gmail.com>
2022-02-23 22:15:38 -0700

    Implement all components into application

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList";
+import Appointment from "./Appointment";
 
 const days = [
   {
@@ -21,10 +22,52 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
 
 export default function Application(props) {
   const [day, setDay] = useState('Monday');
-  const [interviewer, setInterviewer] = useState(1);
+
+  const appointmentItems = appointments.map(appointment => (
+    <Appointment key={appointment.id} {...appointment} />
+  ))
+  appointmentItems.push(<Appointment key='last' time='5pm' />)
 
   return (
     <main className="layout">
@@ -49,7 +92,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointmentItems}
       </section>
     </main>
   );
